1c856877bbf7db98da3f348b0847c0aa160fda35
Ammar Omer <amar@livestreamlines.com>
2024-12-16 01:25:19 +0400

    smalladjustments

--- src/app/services/developer.service.ts
+++ src/app/services/developer.service.ts
@@ -12,7 +12,7 @@ import { AuthService } from './auth.service';  // Import AuthService to access t
 })
 export class DeveloperService {
   //private apiUrl = 'https://lslcloud.com/api/main/developers';  // API URL for fetching developers
-  private apiUrl = environment.apiUrl + +'/developers'; 
+  private apiUrl = environment.apiUrl + '/developers'; 
   private baseUrl = environment.apiUrl + '/developers'; 
 
  // private baseUrl = 'https://liveview.lslcloud.com/api/admin';
