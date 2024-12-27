d567638cd8892b17da600b50a3702ce839a93898
Ammar Omer <amar@livestreamlines.com>
2024-12-27 04:28:42 +0400

    commondatepicker

--- src/app/components/camera-compare-side/camera-compare-side.component.ts
+++ src/app/components/camera-compare-side/camera-compare-side.component.ts
@@ -56,8 +56,6 @@ export class CameraCompareSideComponent implements OnInit {
       this.date1Pictures = data.date1Photos.map(photo => photo.toString());
       this.date2Pictures = data.date2Photos.map(photo => photo.toString());
       this.path = data.path;
-     // console.log("Date 1 Pictures:", this.date1Pictures);  // Debug log
-     // console.log("Date 2 Pictures:", this.date2Pictures);  // Debug log
       // Set default selected pictures
       if (this.date1Pictures.length > 0) {
         const lastPhotoDate1 = this.date1Pictures[this.date1Pictures.length - 1];
@@ -82,16 +80,6 @@ export class CameraCompareSideComponent implements OnInit {
     return new Date(year, month, day);
   }
 
-
-  formatTimestamp(timestamp: string): string {
-    const year = timestamp.substring(0, 4);
-    const month = timestamp.substring(4, 6);
-    const day = timestamp.substring(6, 8);
-    const hour = timestamp.substring(8, 10);
-    const minute = timestamp.substring(10, 12);
-    const second = timestamp.substring(12, 14);
-    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
-  }
 
   onDate1PictureSelect(picture: string): void {
     //this.loadingLeft = true;
@@ -130,14 +118,6 @@ export class CameraCompareSideComponent implements OnInit {
     return `${this.path}/large/${picture}.jpg` ;
   }
 
-  // Method to get thumbnail URL
-  getThumbPictureUrl(picture: string): string {
-    //return `https://lslcloud.com/photos/${this.developerTag}/${this.projectTag}/${this.cameraName}/thumbs/${picture}.jpg`;
-    return `${this.path}/thumbs/${picture}.jpg` ;
-
-  }
-
-
   onDate1Change(newDate: string): void {
     // const inputElement = event.target as HTMLInputElement;
     const selectedDate = new Date(newDate); // Parse the date
@@ -171,17 +151,5 @@ formatDate(date: Date): string {
   const day = date.getDate().toString().padStart(2, '0');
   return `${year}${month}${day}`;
 }
-
-  // Method to update slider overlay based on sliderPosition
-  updateSlider(): void {
-    const leftImg = document.querySelector('.left-img') as HTMLElement;
-    const rightImg = document.querySelector('.right-img') as HTMLElement;
-    leftImg.style.clipPath = `inset(0 ${100 - this.sliderPosition}% 0 0)`;
-    rightImg.style.clipPath = `inset(0 0 0 ${this.sliderPosition}%)`;
-    // if (leftImg && rightImg) {
-    //   leftImg.style.width = `${this.sliderPosition}%`;
-    //   rightImg.style.width = `${100 - this.sliderPosition}%`;
-    // }
-  }
 
 }
