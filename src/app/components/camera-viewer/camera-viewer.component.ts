b0752cf61c8b22019984a259cfa4cbee1e6448f4
Ammar Omer <amar@livestreamlines.com>
2025-01-09 18:31:56 +0400

    monitoradjust22

--- src/app/components/camera-viewer/camera-viewer.component.ts
+++ src/app/components/camera-viewer/camera-viewer.component.ts
@@ -32,34 +32,24 @@ export class CameraViewerComponent implements OnInit {
 
   groupByDeveloperAndProject(cameras: any[]): any {
     return cameras.reduce((acc: GroupedCameras, camera) => {
-      const regex = /^([^/]+)\/([^/]+)\/([^()]+)\(([^)]+)\)$/;
-      const match = camera.FullName.match(regex);
-      if (match) {
-        const [_, developer, project, cameraName, serverFolder] = match;
-        camera.developer = developer;
-        camera.project = project;
-        camera.cameraName = cameraName;
-        camera.serverFolder = serverFolder;
-  
-        // Group by developer and project
-        acc[developer] = acc[developer] || {};
-        acc[developer][project] = acc[developer][project] || [];
-        acc[developer][project].push(camera);
-      }
+      const developer = camera.developer;
+      const project = camera.project;
+      const cameraName = camera.cameraName;
+      const serverFolder = camera.serverFolder;
+      // Group by developer and project
+      acc[developer] = acc[developer] || {};
+      acc[developer][project] = acc[developer][project] || [];
+      acc[developer][project].push(camera);
       return acc;
     }, {});
   }
 
   getImagePath(camera: any): string {
-    const [developer, project, cameraName] = camera.FullName.split('/');
-    const date = new Date(camera.lastPhoto);
-    const formattedDate = `${date.getFullYear()}${(date.getMonth() + 1)
-      .toString().padStart(2, '0')}${date.getDate()
-      .toString().padStart(2, '0')}${date.getHours()
-      .toString().padStart(2, '0')}${date.getMinutes()
-      .toString().padStart(2, '0')}${date.getSeconds()
-      .toString().padStart(2, '0')}`;
-    return `${environment.backend}/media/upload/${developer}/${project}/${cameraName}/large/${formattedDate}.jpg`;
+    const developer = camera.developer;
+    const project = camera.project;
+    const cameraName = camera.cameraName;
+    const lastPhoto = camera.lastPhoto;
+    return `${environment.backend}/media/upload/${developer}/${project}/${cameraName}/large/${lastPhoto}`;
   }
 
   isUpdated(lastPhoto: string): boolean {
