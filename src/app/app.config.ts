5d45e70b0deef4cfd1d02b5ac79e8b39d5cb09ed
Ammar Omer <amar@livestreamlines.com>
2025-11-06 10:58:26 +0400

    logout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';  // Import HttpClient
+import { provideHttpClient, withInterceptors } from '@angular/common/http';  // Import HttpClient
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync()
+    provideHttpClient(withInterceptors([authInterceptor])), provideAnimationsAsync()
   ]
 };
